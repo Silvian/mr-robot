3bb81021f7b210b1b6e05024778b4d6c21d7e025
Silvian <silvian.dragan@gmail.com>
2020-02-09 18:02:15 +0000

    implemented ci plugin

---
+++ plugins/continuous-deployment/continuousdeployment.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+from errbot import BotPlugin, botcmd
+
+
+class ContinuousDeployment(BotPlugin):
+    """Bot which when asked will perform release to production."""
+
+    RELEASE_SCRIPT = os.environ.get("RELEASE_SCRIPT", default=None)
+
+    @botcmd
+    def release_production(self, msg, args):
+        if not self.RELEASE_SCRIPT:
+            return "No release script configured. :persevere:"
+
+        try:
+            subprocess.call([self.RELEASE_SCRIPT])
+        except OSError:
+            return "Failed to run release script... :disappointed:"
+
+        return "Releasing to production! :sunglasses:"
